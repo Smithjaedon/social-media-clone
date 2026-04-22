f82209f69652bf5c9b52173d5b85eadec2992d41
Jaedon Smith <86936528+Smithjaedon@users.noreply.github.com>
2026-04-22 18:36:44 -0400

    made the like command more random in numbers

--- posts/management/commands/populate_likes.py
+++ posts/management/commands/populate_likes.py
@@ -1,3 +1,4 @@
+import random
 from django.core.management.base import BaseCommand
 from posts.models import User, Post, Like
 from django.db.models import F
@@ -6,11 +7,12 @@ class Command(BaseCommand):
     help = 'Populate the database with fake likes'
 
     def handle(self, *args, **options):
+        users = list(User.objects.all())
+        posts = Post.objects.all()
         
-        posts = Post.objects.all().order_by('?')
         for post in posts:
-            for _ in range(10):
-                user = User.objects.order_by('?').first()
+            random_users = random.sample(users, k=random.randint(1, min(len(users), 50)))
+            for user in random_users:
                 like, created = Like.objects.get_or_create(user=user, post=post)
                 if created:
                     Post.objects.filter(id=post.id).update(like_count=F('like_count') + 1)
